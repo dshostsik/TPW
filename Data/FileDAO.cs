4b25f4cc108d064006886f322a28db6da9c0ebe2
dshostsik <248680@edu.p.lodz.pl>
2024-06-02 00:49:10 +0300

    commiting dao file

---
+++ Data/FileDAO.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.Concurrent;
+using System.Text.Json;
+
+namespace Data
+{
+    internal class FileDAO : IDisposable
+    {
+        private Task Logger;
+        private StreamWriter Writer;
+        private BlockingCollection<BallData> Queue;
+        private string Path = "../../../..Data/log.txt";
+        private int Width;
+        private int Height;
+
+        public FileDAO(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+
+        public void FillQueue(IBall iball)
+        {
+            if (iball == null)
+            {
+                return;
+            }
+
+            BallData servedBall = new BallData(iball.position.X, iball.position.Y, iball.radius, iball.weight,iball.speed.X,iball.speed.Y, iball.number);
+            if (!Queue.IsAddingCompleted && !Queue.IsCompleted)
+            {
+                Queue.Add(servedBall);
+            }
+        }
+
+        private void write()
+        {
+            using (Writer = new StreamWriter(Path, append:false))
+            {
+                JsonSerializerOptions jso = new JsonSerializerOptions();
+                jso.WriteIndented = true;
+                Writer.Write("\n");
+                Writer.Write("{" + string.Format("\n\t\"Width\": {0},\n\t\"Height\": {1}\n", Width, Height) + "}");
+                foreach (BallData ball in Queue.GetConsumingEnumerable())
+                {
+                    string log = JsonSerializer.Serialize(ball, jso);
+                    
+                    Writer.Write("," + "\n" + log);
+                }
+                Writer.Write("\n}");
+                Writer.Flush();
+            }
+        }
+        
+        public void Dispose()
+        {
+            Queue.CompleteAdding();
+            Logger.Wait();
+            Logger.Dispose();
+        }
+        
+        internal class BallData
+        {
+            public float X { get; set; }
+            public float Y { get; set; }
+            public float SpeedX { get; set; }
+            public float SpeedY { get; set; }
+            public int Weight { get; set; }
+            public int Radius { get; set; }
+            public string Time { get; set; }
+            public int Number { get; set; }
+
+            public BallData(float x, float y, int radius, int weight, float speedX, float speedY,  int number)
+            {
+                X = x;
+                Y = y;
+                SpeedX = speedX;
+                SpeedY = speedY;
+                Weight = weight;
+                Radius = radius;
+                Number = number;
+                Time = DateTime.UtcNow.ToString("G");
+            }
+        }
+    }    
+}
+
